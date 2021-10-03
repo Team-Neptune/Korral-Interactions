1c8c9ae198181de1bfa997e4b2ea3cba46008493
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-10-02 17:24:17 -0700

    Add option for authorized users to close ticket

---
+++ src/buttons/close_ticket_.ts
@@ -0,0 +1,32 @@
+import ButtonCommand from "../classes/ButtonCommand";
+
+export default new ButtonCommand({
+    checkType:"STARTS_WITH",
+    execute(interaction){
+        let userId = interaction.data.custom_id.split("close_ticket_")[1];
+        let currentUserId = interaction.member.user.id;
+        let threadChannelId = interaction.message.channel_id;
+        if(currentUserId != userId && !interaction.member.roles.includes(interaction.internalBot.config.supportRoleId))
+            return interaction.reply({
+                content:`This isn't a ticket you can manage.`,
+                ephemeral:true
+            })
+        interaction.reply({
+            content:`<#${threadChannelId}> will be locked soon.`,
+            ephemeral:true
+        })
+        .then(() => {
+            interaction.sendMessage(threadChannelId, {
+                embeds:[
+                    {
+                        "description":`🔒 Ticket has been closed by <@${currentUserId}>`,
+                        "color":16711680
+                    }
+                ]
+            })
+            .then(() => {
+                return interaction.lockThread(threadChannelId)
+            })
+        })
+    }
+})
--- src/commands/ticket.ts
+++ src/commands/ticket.ts
@@ -17,7 +17,23 @@ export default new Command({
                 interaction.joinThread(channel.id).then(res => {
                     interaction.sendMessage(channel.id, {
                         "content":
-                        `Hey <@${interaction.member.user.id}>,\n<@&${interaction.internalBot.config.supportRoleId}> will be here to support you shortly. In the meantime, to make it easier for us and others help you with your issue, please tell us a few things about your setup, like:\n\n- Firmware and CFW / Atmosphere / DeepSea version\n- Do you use hekate or fusee-primary?\n- If you have an error screen with ID or code, what does it say? A screenshot/picture could be helpful.\n- What, if anything, have you tried to fix the issue?\n\n*(Disclaimer: You may not receive an answer instantly. Many of us have lives outside of Discord and will respond whenever we're able to, whenever that is.)*`
+                        `Hey <@${interaction.member.user.id}>,\n<@&${interaction.internalBot.config.supportRoleId}> will be here to support you shortly. In the meantime, to make it easier for us and others help you with your issue, please tell us a few things about your setup, like:\n\n- Firmware and CFW / Atmosphere / DeepSea version\n- Do you use hekate or fusee-primary?\n- If you have an error screen with ID or code, what does it say? A screenshot/picture could be helpful.\n- What, if anything, have you tried to fix the issue?\n\n*(Disclaimer: You may not receive an answer instantly. Many of us have lives outside of Discord and will respond whenever we're able to, whenever that is.)*`,
+                        "components":[
+                            {
+                                "type":1,
+                                "components":[
+                                    {
+                                        "type":2,
+                                        "style":2,
+                                        "custom_id":`close_ticket_${interaction.member.user.id}`,
+                                        "label":"Close Ticket",
+                                        "emoji":{
+                                            "name":"🔒"
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
                     }).then(r => {
                         interaction.reply({
                             content:`Ticket is ready in <#${channel.id}>`,
