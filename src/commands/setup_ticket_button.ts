75d5028a232bdd385e92049700c6e10da2a52302
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-10-02 20:28:46 -0700

    Update Ticket Creation message

--- src/commands/setup_ticket_button.ts
+++ src/commands/setup_ticket_button.ts
@@ -7,7 +7,7 @@ export default new Command({
             "ephemeral":true
         }).then(() => {
             interaction.sendMessage(interaction.internalBot.config.supportChannelId, {
-                content:"**Check for your issue**\nBefore opening a ticket, check the thread menu <:thread:893969529212928080>, and check the archived tab.\n\n**If you can't find your issue**\nClick the button below **or** run `/ticket` slash command in any other channel to open a support ticket.",
+                content:"**Check for your issue**\nBefore opening a ticket, check the thread menu <:thread:893969529212928080>, and check the archived tab.\n\n**If you can't find your issue**\nClick the button below **or** run `/ticket` slash command in any other channel to open a support ticket.\n\n**Want to help others?**\nOpen the thread menu <:thread:893969529212928080> and search/go through the Active threads for people needing help. Please only join a thread if you intend to provide support.",
                 components:[
                     {
                         type:1,
