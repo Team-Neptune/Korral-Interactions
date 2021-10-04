c5e415e22d186d3c704f31aa11e3667d8b3613b2
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-10-04 15:26:07 -0700

    Add support for "private" tickets

--- src/commands/ticket.ts
+++ src/commands/ticket.ts
@@ -3,7 +3,9 @@ import Command from "../classes/Command";
 export default new Command({
     execute(interaction){
         // 1-90 char only
-        let topic = interaction.data.options.find(o => o.name == "topic")
+        let topic = interaction.data.options.find(o => o.name == "topic");
+        let supportRoleOnly = interaction.data.options.find(o => o.name == "private") || false;
+        let threadStarter = interaction.member.user.id;
         if(topic.value.length > 90 || topic.value.length < 1)
             return interaction.reply({
                 content:`Topic must be 1-90 characters`,
@@ -12,7 +14,7 @@ export default new Command({
         interaction.ack({
             "ephemeral":true
         }).then(() => {
-            interaction.createSupportThread(topic.value, interaction.member.user.id)
+            interaction.createSupportThread(topic.value, threadStarter)
             .then(channel => {
                 //   Error
                 if(typeof channel == 'string')
@@ -23,7 +25,7 @@ export default new Command({
                 interaction.joinThread(channel.id).then(res => {
                     interaction.sendMessage(channel.id, {
                         "content":
-                        `Hey <@${interaction.member.user.id}>,\n<@&${interaction.internalBot.config.supportRoleId}> will be here to support you shortly. In the meantime, to make it easier for us and others help you with your issue, please tell us a few things about your setup, like:\n\n- Firmware and CFW / Atmosphere / DeepSea version\n- Do you use hekate or fusee-primary?\n- If you have an error screen with ID or code, what does it say? A screenshot/picture could be helpful.\n- What, if anything, have you tried to fix the issue?\n\n*(Disclaimer: You may not receive an answer instantly. Many of us have lives outside of Discord and will respond whenever we're able to, whenever that is.)*`,
+                        `Hey <@${threadStarter}>,\n<@&${interaction.internalBot.config.supportRoleId}> will be here to support you shortly. In the meantime, to make it easier for us and others help you with your issue, please tell us a few things about your setup, like:\n\n- Firmware and CFW / Atmosphere / DeepSea version\n- Do you use hekate or fusee-primary?\n- If you have an error screen with ID or code, what does it say? A screenshot/picture could be helpful.\n- What, if anything, have you tried to fix the issue?\n\n*(Disclaimer: You may not receive an answer instantly. Many of us have lives outside of Discord and will respond whenever we're able to, whenever that is.)*\n${supportRoleOnly?"||MARK_PRIVATE_TICKET||":""}`,
                         "components":[
                             {
                                 "type":1,
@@ -31,7 +33,7 @@ export default new Command({
                                     {
                                         "type":2,
                                         "style":2,
-                                        "custom_id":`close_ticket_${interaction.member.user.id}`,
+                                        "custom_id":`close_ticket_${threadStarter}`,
                                         "label":"Close Ticket",
                                         "emoji":{
                                             "name":"🔒"
